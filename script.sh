20fda3fdaeccf2ca1d6cb6e15595013aac37fcdf
frosty322 <frosty3221@mail.ru>
2019-09-27 18:41:59 +0300

    Fixed date and terminal errors

--- script.sh
+++ script.sh
@@ -4,13 +4,17 @@ rm -f ./output.xls
 echo -e "Название файла\tРасширение\tВес\tДата изменения\tДлительность\t\t" >> output.xls
 
 function filesInfo {
+	if [ "$(ls -A "$1" 2> /dev/null)" == "" ]
+	then return
+	else
 	for curFile in "$1"/*
 	do
 		fileNameAbs="${curFile##*/}"
-		if [ -d "$curFile" ]
+		if [[ -d $curFile ]]
 		then
 			filesInfo "$curFile"
-		else 
+		elif [[ -f $curFile ]]
+		then
 			fileName="${fileNameAbs%.[^.]*}"
 			fileExtension="${fileNameAbs##*.}"
 			fileSize=$(wc -c < "$curFile" | awk '{print $1}')
@@ -22,13 +26,13 @@ function filesInfo {
 				fileSize="0"
 			fi
 
-			fileDataOfChange=$(stat -c %y $curFile)
+			fileDataOfChange=$(date +%Y-5m-5d -r "$curFile")
 			duration=$(mediainfo --Inform="General;%Duration%" $curFile)
 			let "fileDuration = duration / 1000"
 			echo -e "$fileName \t $fileExtension \t $fileSize \t $fileDataOfChange \t $fileDuration" >> output.xls
 		fi
 	done
-
+fi
 }
 
 echo "ВВедите путь к каталогу:"
