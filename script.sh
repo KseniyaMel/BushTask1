ef8798303c85a82a62db270e313acf2940034175
frosty322 <frosty3221@mail.ru>
2019-10-01 13:50:28 +0300

    catched wrong directory error

--- script.sh
+++ script.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 
-rm -f ./output.xls
-echo -e "Название файла\tРасширение\tВес\tДата изменения\tДлительность\t\t" >> output.xls
+rm -f ./output.xls #удаление файла, если он есть
+echo -e "Название файла\tРасширение\tВес\tДата изменения\tДлительность\t\t" >> output.xls #название столбцов
 
 function filesInfo {
-	if [ "$(ls -A "$1" 2> /dev/null)" == "" ]
-	then return
+	if [ "$(ls -A "$1" 2> /dev/null)" == "" ] #проверка на наличие директории или файла с таким названием
+	then	
+	return 
 	else
-	for curFile in "$1"/*
+	for curFile in "$1"/* # перебор файлов и деректорий
 	do
-		fileNameAbs="${curFile##*/}"
-		if [[ -d $curFile ]]
+		fileNameAbs="${curFile##*/}" # имя файла из абсолютного пути
+		if [[ -d $curFile ]] # проверка на директорию
 		then
 			filesInfo "$curFile"
-		elif [[ -f $curFile ]]
+		elif [[ -f $curFile ]] #проверка на файл
 		then
-			fileName="${fileNameAbs%.[^.]*}"
-			fileExtension="${fileNameAbs##*.}"
-			fileSize=$(wc -c < "$curFile" | awk '{print $1}')
+			fileName="${fileNameAbs%.[^.]*}" #название файла
+			fileExtension="${fileNameAbs##*.}" #расширение файла
+			fileSize=$(wc -c < "$curFile" | awk '{print $1}') #размер файла
 
-			if [[ "$fileName" == "*" ]]
-			then 
-				fileName="$curFile"
-				fileExtension="пустая папка"
-				fileSize="0"
-			fi
-
-			fileDataOfChange=$(date +%Y-5m-5d -r "$curFile")
-			duration=$(mediainfo --Inform="General;%Duration%" $curFile)
-			let "fileDuration = duration / 1000"
-			echo -e "$fileName \t $fileExtension \t $fileSize \t $fileDataOfChange \t $fileDuration" >> output.xls
+			fileDataOfChange=$(date +%Y-5m-5d -r "$curFile") #дата изменения файла
+			duration=$(mediainfo --Inform="General;%Duration%" $curFile) #длительность медиафайлов
+			let "fileDuration = duration / 1000" #перевод длительности в секунды
+			echo -e "$fileName \t $fileExtension \t $fileSize \t $fileDataOfChange \t $fileDuration" >> output.xls #вывод данных
 		fi
 	done
 fi
@@ -37,4 +31,9 @@ fi
 
 echo "ВВедите путь к каталогу:"
 read input
-filesInfo "$input"
+if [ -d $input ]
+then
+filesInfo "$input" #инициализация функции
+else
+	echo "Неверная директория"
+fi
